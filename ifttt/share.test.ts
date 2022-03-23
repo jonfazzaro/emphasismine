efc46f5250b4eca96e2e101156cbc522db24f45f
Jon Fazzaro <jon@fazzaro.com>
2022-03-23 09:25:17 -0400

    feat: optional LI posting

--- ifttt/share.test.ts
+++ ifttt/share.test.ts
@@ -3,7 +3,8 @@ const share = require("./share");
 describe("The share filter", () => {
   it("given nothing returns nothing", () => {
     post("", "", "");
-    expect(Twitter.postNewTweet.setTweet).toHaveBeenCalledWith("");
+    expectTweet("");
+    expectNoLinkedInPost();
   });
 
   it("removes the top of the link post", () => {
@@ -12,16 +13,57 @@ describe("The share filter", () => {
       "https://www.amazon.com",
       "buy buy,emphasismine,get back"
     );
-    expect(Twitter.postNewTweet.setTweet).toHaveBeenCalledWith(
+    expectTweet(
       `"Unfortunately, the Scrum community seems unwilling to inspect and adapt. It clings to a Product Owner ideal that is rarely used in practice and doesn’t scale. It’s easier to point fingers and claim that people with PM/PO solutions or multiple POs per product are not doing Scrum."\n\nhttps://www.amazon.com #buybuy #emphasismine #getback`
     );
+    expectNoLinkedInPost();
+  });
+
+  describe("given a tag of li", () => {
+    it("posts to LinkedIn, too", () => {
+      post(
+        `"Just what do you think you are you doing, Dave?"`,
+        "https://www.hal9000.net/daisy",
+        "agility,in,horse feathers"
+      );
+
+      expectTweet(
+        `"Just what do you think you are you doing, Dave?"\n\nhttps://www.hal9000.net/daisy #agility #horsefeathers`
+      );
+      expectLinkedInPost(
+        "https://www.hal9000.net/daisy",
+        `"Just what do you think you are you doing, Dave?"\n\n#agility #horsefeathers`
+      );
+    });
   });
 });
 
 const Twitter = { postNewTweet: { setTweet: jest.fn() } };
+const Linkedin = {
+  shareLink: {
+    setLinkUrl: jest.fn(),
+    setComment: jest.fn(),
+    skip: jest.fn(),
+  },
+};
+
+function expectLinkedInPost(link: string, comment: string) {
+  expect(Linkedin.shareLink.setLinkUrl).toHaveBeenCalledWith(link);
+  expect(Linkedin.shareLink.setComment).toHaveBeenCalledWith(comment);
+}
 
 function post(body: string, url: string, tags: string) {
-  share(tumblr({ body, url, tags }), Twitter);
+  share(tumblr({ body, url, tags }), Twitter, Linkedin);
+}
+
+function expectTweet(tweet) {
+  expect(Twitter.postNewTweet.setTweet).toHaveBeenCalledWith(tweet);
+}
+
+function expectNoLinkedInPost() {
+  expect(Linkedin.shareLink.skip).toHaveBeenCalled();
+  expect(Linkedin.shareLink.setLinkUrl).not.toHaveBeenCalled();
+  expect(Linkedin.shareLink.setComment).not.toHaveBeenCalled();
 }
 
 function tumblr({ body, url, tags }) {
--- ifttt/share.ts
+++ ifttt/share.ts
@@ -1,7 +1,15 @@
-module.exports = function share(Tumblr:any, Twitter:any) {
-  Twitter.postNewTweet.setTweet(content());
+module.exports = function share(Tumblr: any, Twitter: any, Linkedin: any) {
+  const IN = "in";
+  Twitter.postNewTweet.setTweet(tweet());
+  if (isProfessional(Tumblr.newLinkPost.PostTags)) shareToLinkedIn();
+  else Linkedin.shareLink.skip();
 
-  function content() {
+  function shareToLinkedIn() {
+    Linkedin.shareLink.setLinkUrl(Tumblr.newLinkPost.LinkUrl);
+    Linkedin.shareLink.setComment(linkedInComment());
+  }
+
+  function tweet() {
     const body = stripHeader(Tumblr.newLinkPost.PostBodyHtml);
     const link = Tumblr.newLinkPost.LinkUrl;
     const tags = hash(Tumblr.newLinkPost.PostTags);
@@ -9,10 +17,16 @@ module.exports = function share(Tumblr:any, Twitter:any) {
     return `${body}\n\n${link} ${tags}`.trim();
   }
 
+  function linkedInComment() {
+    const body = stripHeader(Tumblr.newLinkPost.PostBodyHtml);
+    const tags = hash(Tumblr.newLinkPost.PostTags);
+
+    return `${body}\n\n${tags}`.trim();
+  }
+
   function stripHeader(body: string) {
     const P = "</p>";
-    if (body.indexOf(P) === -1)
-      return body;
+    if (body.indexOf(P) === -1) return body;
 
     return body.split(P).slice(1).join("").trim();
   }
@@ -22,7 +36,12 @@ module.exports = function share(Tumblr:any, Twitter:any) {
 
     return tags
       .split(",")
+      .filter(t => t != IN)
       .map(t => "#" + t.replace(/\s/g, ""))
       .join(" ");
+  }
+
+  function isProfessional(tags: string) {
+    return !!tags.split(",").filter(t => t === IN).length;
   }
 };
