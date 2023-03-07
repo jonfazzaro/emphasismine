f5ef7f6bae9e3d5c580b00eac119aa5994e87357
Jon Fazzaro <jon@fazzaro.com>
2023-03-07 14:37:12 -0500

    feat: debug mode

--- ifttt/share.test.ts
+++ ifttt/share.test.ts
@@ -6,17 +6,19 @@ describe("The share filter", () => {
     expectToot("");
     expectTweet("");
     expectNoLinkedInPost();
+    expectNoSmsMessage();
   });
 
   it("removes the top of the link post", () => {
     post(
-      `<p>Scrum's unintended and gradual disconnect from Product Management</p>\n&ldquo;Unfortunately, the Scrum community seems unwilling to inspect and adapt. It clings to a Product Owner ideal that is rarely used in practice and doesn&rsquo;t scale. It&lsquo;s easier to point fingers and claim that people with PM/PO solutions or multiple POs per product are not doing Scrum.&rdquo;\n\n`,
+      `<p>Scrum's unintended and gradual disconnect from Product Management</p>\n<p>&ldquo;Unfortunately, the Scrum community seems unwilling to inspect and adapt. It clings to a Product Owner ideal that is rarely used in practice and doesn&rsquo;t scale. It&lsquo;s easier to point fingers and claim that people with PM/PO solutions or multiple POs per product are not doing Scrum.&rdquo;</p>\n\n`,
       "https://www.amazon.com",
       "buy buy,get back"
     );
     expectTweet(`"Unfortunately, the Scrum community seems unwilling to inspect and adapt. It clings to a Product Owner ideal that is rarely used in practice and doesn't scale. It's easier to point fingers and claim that people with PM/PO solutions or mu..."\n\nhttps://www.amazon.com #buybuy #getback`);
     expectToot(`"Unfortunately, the Scrum community seems unwilling to inspect and adapt. It clings to a Product Owner ideal that is rarely used in practice and doesn't scale. It's easier to point fingers and claim that people with PM/PO solutions or multiple POs per product are not doing Scrum."\n\nhttps://www.amazon.com #buybuy #getback`);
     expectNoLinkedInPost();
+    expectNoSmsMessage();
   });
 
   describe("given a professional signal tag", () => {
@@ -33,13 +35,14 @@ describe("The share filter", () => {
         "https://www.hal9000.net/daisy",
         `"Just what do you think you are you doing, Dave?"\n\n#agility #horsefeathers`
       );
+      expectNoSmsMessage();
     });
   });
 
   describe('given this post HTML', () => {
     it('should post to LinkedIn', () => {
       const HTML = `<p class="npf_link" data-npf='{"type":"link","url":"https://stackoverflow.blog/2023/02/13/coding-102-writing-code-other-people-can-read/","display_url":"https://stackoverflow.blog/2023/02/13/coding-102-writing-code-other-people-can-read/","title":"Coding 102: Writing code other people can read","poster":[{"media_key":"a4446df772d8a5a54a918af0f66d6085:a98981dbd19f9ffe-23","type":"image/jpeg","width":2560,"height":1344}]}'><a href="https://stackoverflow.blog/2023/02/13/coding-102-writing-code-other-people-can-read/" target="_blank">Coding 102: Writing code other people can read</a></p><p>"It took me two hours to code the script, but eight hours to undo the damage it caused and fix my convoluted code. That&rsquo;s the maintenance cost."</p>`
-      let url = "https://stackoverflow.blog/2023/02/13/coding-102-writing-code-other-people-can-read/";
+      const url = "https://stackoverflow.blog/2023/02/13/coding-102-writing-code-other-people-can-read/";
       post(
           HTML,
           url,
@@ -52,21 +55,37 @@ describe("The share filter", () => {
     });
   });
 
-  describe('given some other post HTML', () => {
+  describe('when debugging', () => {
+    const HTML = `<p class="npf_link" data-npf='{"type":"link","url":"https://cabel.com/2023/02/25/the-courtyard/","display_url":"https://cabel.com/2023/02/25/the-courtyard/","title":"The Courtyard","description":"There was a time when apartment buildings were built with large, spacious courtyards. The idea is so obvious: it&rsquo;s not comfortable or natural to live in a packed block of living units. Breath&hellip;","poster":[{"media_key":"197192bdb19ab070cd8e67dd171e02ce:80ba8e5b152836bd-c6","type":"image/jpeg","width":1200,"height":900}]}'><a href="https://cabel.com/2023/02/25/the-courtyard/" target="_blank">The Courtyard</a></p><p>"Whatever you're working on right now, whatever it might be, I ask: try to leave a little space for a courtyard."</p>`
+    const url = "https://seths.blog/2023/02/checking-the-date/";
 
-    it('should post like this to LinkedIn', () => {
-      const HTML = `<p class="npf_link" data-npf='{"type":"link","url":"https://seths.blog/2023/02/checking-the-date/","display_url":"https://seths.blog/2023/02/checking-the-date/","title":"Checking the date","poster":[{"media_key":"60e6468e3fbe6b6e0345f1542ab661af:45eb4eb6fd1ff38a-d0","type":"image/jpeg","width":1200,"height":628}]}'><a href="https://seths.blog/2023/02/checking-the-date/" target="_blank">Checking the date</a></p><p>"Every day is April Fools from now on."</p>`
-      let url = "https://seths.blog/2023/02/checking-the-date/";
-      post(
-          HTML,
-          url,
-          "in,ai"
-      );
-      expectLinkedInPost(
-          url,
-          `"Every day is April Fools from now on."\n\n#ai`
-      );
+    beforeEach(() => {
+      Sms.sendMeText.skip.mockClear();
+      debugPost(HTML, url, "in,test")
+    })
+
+    it('should not post anywhere', function () {
+      expect(Twitter.postNewTweet.skip).toHaveBeenCalled();
+      expect(MakerWebhooks.makeWebRequest.skip).toHaveBeenCalled();
+      expect(Linkedin.shareLink.skip).toHaveBeenCalled();
     });
+
+    it('should send SMS messages', function () {
+      expect(Sms.sendMeText.skip).not.toHaveBeenCalled()
+    });
+
+    it('should SMS the Tweet', function () {
+      expect(Sms.sendMeText.setMessage).toHaveBeenCalledWith(`Tweet: "Whatever you're working on right now, whatever it might be, I ask: try to leave a little space for a courtyard."\n\n${url} #test`)
+    });
+
+    it('should SMS the Toot', function () {
+      expect(Sms.sendMeText.setMessage).toHaveBeenCalledWith(`Toot: "Whatever you're working on right now, whatever it might be, I ask: try to leave a little space for a courtyard."\n\n${url} #test`)
+    });
+
+    it('should SMS the LinkedIn comment', function () {
+      expect(Sms.sendMeText.setMessage).toHaveBeenCalledWith(`LinkedIn: "Whatever you're working on right now, whatever it might be, I ask: try to leave a little space for a courtyard."\n\n#test`)
+    });
+
   });
 });
 
@@ -77,9 +96,15 @@ const MakerWebhooks = {
     setContentType: jest.fn(),
     setAdditionalHeaders: jest.fn(),
     setBody: jest.fn(),
+    skip: jest.fn()
   }
 }
-const Twitter = { postNewTweet: { setTweet: jest.fn() } };
+const Twitter = {
+  postNewTweet: {
+    setTweet: jest.fn(),
+    skip: jest.fn()
+  }
+};
 const Linkedin = {
   shareLink: {
     setLinkUrl: jest.fn(),
@@ -87,6 +112,12 @@ const Linkedin = {
     skip: jest.fn(),
   },
 };
+const Sms = {
+  sendMeText: {
+    skip: jest.fn(),
+    setMessage: jest.fn()
+  }
+}
 
 function expectLinkedInPost(link: string, comment: string) {
   expect(Linkedin.shareLink.setLinkUrl).toHaveBeenCalledWith(link);
@@ -94,7 +125,20 @@ function expectLinkedInPost(link: string, comment: string) {
 }
 
 function post(body: string, url: string, tags: string) {
-  share(tumblr({ body, url, tags }), Twitter, Linkedin, MakerWebhooks, "hachyderm.io", "t00ts");
+  share(
+      tumblr({ body, url, tags }),
+      Twitter, Linkedin, MakerWebhooks, Sms,
+      "hachyderm.io",
+      "t00ts");
+}
+
+function debugPost(body: string, url: string, tags: string) {
+  share(
+      tumblr({ body, url, tags }),
+      Twitter, Linkedin, MakerWebhooks, Sms,
+      "hachyderm.io",
+      "t00ts",
+      true);
 }
 
 function expectToot(toot) {
@@ -103,16 +147,22 @@ function expectToot(toot) {
   expect(MakerWebhooks.makeWebRequest.setContentType).toHaveBeenCalledWith("application/json");
   expect(MakerWebhooks.makeWebRequest.setAdditionalHeaders).toHaveBeenCalledWith("Authorization: Bearer t00ts");
   expect(MakerWebhooks.makeWebRequest.setBody) .toHaveBeenCalledWith(JSON.stringify({ status: toot }));
+  expect(MakerWebhooks.makeWebRequest.skip).not.toHaveBeenCalled()
 }
 
 function expectTweet(tweet) {
   expect(Twitter.postNewTweet.setTweet).toHaveBeenCalledWith(tweet);
+  expect(Twitter.postNewTweet.skip).not.toHaveBeenCalled()
 }
 
 function expectNoLinkedInPost() {
   expect(Linkedin.shareLink.skip).toHaveBeenCalled();
   expect(Linkedin.shareLink.setLinkUrl).not.toHaveBeenCalled();
   expect(Linkedin.shareLink.setComment).not.toHaveBeenCalled();
+}
+
+function expectNoSmsMessage() {
+  expect(Sms.sendMeText.skip).toHaveBeenCalled();
 }
 
 function tumblr({ body, url, tags }) {
--- ifttt/share.ts
+++ ifttt/share.ts
@@ -1,111 +1,139 @@
 module.exports = function share(
-  Tumblr: any,
-  Twitter: any,
-  Linkedin: any,
-  MakerWebhooks: any,
-  MASTODON_SERVER: string,
-  MASTODON_TOKEN: string
+    Tumblr: any,
+    Twitter: any,
+    Linkedin: any,
+    MakerWebhooks: any,
+    Sms: any,
+    MASTODON_SERVER: string,
+    MASTODON_TOKEN: string,
+    DEBUG: boolean = false
 ) {
-  const IN = "in";
+    const IN = "in";
 
-  Twitter.postNewTweet.setTweet(tweet());
-  shareToMastodon();
-  if (isProfessional(Tumblr.newLinkPost.PostTags)) shareToLinkedIn();
-  else Linkedin.shareLink.skip();
+    (function () {
+        if (DEBUG)
+            return debug();
 
-  function shareToMastodon() {
-    post(`https://${MASTODON_SERVER}/api/v1/statuses`, {
-      status: toot(),
-    });
-  }
+        Sms.sendMeText.skip();
+        Twitter.postNewTweet.setTweet(tweet());
+        shareToMastodon();
+        if (isProfessional(Tumblr.newLinkPost.PostTags)) shareToLinkedIn();
+        else Linkedin.shareLink.skip();
 
-  function post(url: string, body: any) {
-    MakerWebhooks.makeWebRequest.setUrl(url);
-    MakerWebhooks.makeWebRequest.setMethod("POST");
-    MakerWebhooks.makeWebRequest.setContentType("application/json");
-    MakerWebhooks.makeWebRequest.setAdditionalHeaders(
-      `Authorization: Bearer ${MASTODON_TOKEN}`
-    );
-    MakerWebhooks.makeWebRequest.setBody(JSON.stringify(body));
-  }
+        function tweet() {
+            const tags = hash(Tumblr.newLinkPost.PostTags);
+            return content(
+                truncated(clean(Tumblr.newLinkPost.PostBodyHtml), tags),
+                Tumblr.newLinkPost.LinkUrl,
+                tags);
+        }
 
-  function shareToLinkedIn() {
-    Linkedin.shareLink.setLinkUrl(Tumblr.newLinkPost.LinkUrl);
-    Linkedin.shareLink.setComment(linkedInComment());
-  }
+        function shareToMastodon() {
+            post(`https://${MASTODON_SERVER}/api/v1/statuses`, {
+                status: toot(),
+            });
+        }
 
-  function toot() {
-    const tags = hash(Tumblr.newLinkPost.PostTags);
-    const body = fixQuotes(stripHtml(stripHeader(Tumblr.newLinkPost.PostBodyHtml)));
-    const link = Tumblr.newLinkPost.LinkUrl;
+        function shareToLinkedIn() {
+            Linkedin.shareLink.setLinkUrl(Tumblr.newLinkPost.LinkUrl);
+            Linkedin.shareLink.setComment(linkedInComment());
+        }
 
-    return `${body}\n\n${link} ${tags}`.trim();
-  }
+        function toot() {
+            return content(
+                clean(Tumblr.newLinkPost.PostBodyHtml),
+                Tumblr.newLinkPost.LinkUrl,
+                hash(Tumblr.newLinkPost.PostTags));
+        }
 
-  function tweet() {
-    const tags = hash(Tumblr.newLinkPost.PostTags);
-    const body = truncated(fixQuotes(stripHeader(Tumblr.newLinkPost.PostBodyHtml)), tags);
-    const link = Tumblr.newLinkPost.LinkUrl;
+        function linkedInComment() {
+            return comment(
+                clean(Tumblr.newLinkPost.PostBodyHtml),
+                hash(Tumblr.newLinkPost.PostTags));
+        }
 
-    return `${body}\n\n${link} ${tags}`.trim();
-  }
+        function post(url: string, body: any) {
+            MakerWebhooks.makeWebRequest.setUrl(url);
+            MakerWebhooks.makeWebRequest.setMethod("POST");
+            MakerWebhooks.makeWebRequest.setContentType("application/json");
+            MakerWebhooks.makeWebRequest.setAdditionalHeaders(
+                `Authorization: Bearer ${MASTODON_TOKEN}`
+            );
+            MakerWebhooks.makeWebRequest.setBody(JSON.stringify(body));
+        }
 
-  function truncated(body: string, tags: string) {
-    const overage = computeOverage(body, tags);
-    if (body.length && overage > 0) return truncate(body, overage, `..."`);
+        function content(body: string, link: string, tags: string) {
+            return `${body}\n\n${link} ${tags}`.trim();
+        }
 
-    return body;
-  }
+        function comment(body: string, tags: string) {
+            return `${body}\n\n${tags}`.trim();
+        }
 
-  function truncate(value: string, by: number, cap: string) {
-    return value.substring(0, value.length - (by + cap.length)) + cap;
-  }
+        function truncated(body: string, tags: string) {
+            const overage = computeOverage(body, tags);
+            if (body.length && overage > 0) return truncate(body, overage, `..."`);
 
-  function computeOverage(body: string, tags: string) {
-    const TWITTER_LIMIT = 280;
-    const TWITTER_LINK_LENGTH = 23; // per https://help.twitter.com/en/using-twitter/how-to-tweet-a-link
-    const limit = TWITTER_LIMIT - TWITTER_LINK_LENGTH;
-    const length = body.length + tags.length;
-    return length - limit;
-  }
+            return body;
+        }
 
-  function linkedInComment() {
-    const body = stripHtml(fixQuotes(stripHeader(Tumblr.newLinkPost.PostBodyHtml)));
-    const tags = hash(Tumblr.newLinkPost.PostTags);
+        function truncate(value: string, by: number, cap: string) {
+            return value.substring(0, value.length - (by + cap.length)) + cap;
+        }
 
-    return `${body}\n\n${tags}`.trim();
-  }
+        function computeOverage(body: string, tags: string) {
+            const TWITTER_LIMIT = 280;
+            const TWITTER_LINK_LENGTH = 23; // per https://help.twitter.com/en/using-twitter/how-to-tweet-a-link
+            const limit = TWITTER_LIMIT - TWITTER_LINK_LENGTH;
+            const length = body.length + tags.length;
+            return length - limit;
+        }
 
-  function stripHeader(body: string) {
-    const P = "</p>";
-    if (body.indexOf(P) === -1) return body;
+        function clean(html: string) {
+            return stripHtml(fixQuotes(stripHeader(html)));
+        }
 
-    return body.split(P).slice(1).join("").trim();
-  }
+        function stripHeader(body: string) {
+            const P = "</p>";
+            if (body.indexOf(P) === -1) return body;
 
-  function stripHtml(html: string) {
-    return html.replace(/<\/?[^>]+(>|$)/g, "")
-  }
+            return body.split(P).slice(1).join("").trim();
+        }
 
-  function fixQuotes(content: string) {
-    return content
-      .replace(/&ldquo;/g, `"`)
-      .replace(/&rdquo;/g, `"`)
-      .replace(/&rsquo;/g, `'`)
-      .replace(/&lsquo;/g, `'`)
-  }
+        function stripHtml(html: string) {
+            return html.replace(/<\/?[^>]+(>|$)/g, "")
+        }
 
-  function hash(tags: string) {
-    if (!tags) return "";
+        function fixQuotes(content: string) {
+            return content
+                .replace(/&ldquo;/g, `"`)
+                .replace(/&rdquo;/g, `"`)
+                .replace(/&rsquo;/g, `'`)
+                .replace(/&lsquo;/g, `'`)
+        }
 
-    return tags
-      .split(",")
-      .filter(t => t != IN)
-      .map(t => "#" + t.replace(/\s/g, ""))
-      .join(" ");
-  }
+        function hash(tags: string) {
+            if (!tags) return "";
 
-  function isProfessional(tags: string) {
-    return !!tags.split(",").filter(t => t === IN).length;
-  }
+            return tags
+                .split(",")
+                .filter(t => t != IN)
+                .map(t => "#" + t.replace(/\s/g, ""))
+                .join(" ");
+        }
+
+        function isProfessional(tags: string) {
+            return !!tags.split(",").filter(t => t === IN).length;
+        }
+
+        function debug() {
+            Twitter.postNewTweet.skip();
+            MakerWebhooks.makeWebRequest.skip();
+            Linkedin.shareLink.skip();
+            Sms.sendMeText.setMessage(`Tweet: ${tweet()}`);
+            Sms.sendMeText.setMessage(`Toot: ${toot()}`);
+            Sms.sendMeText.setMessage(`LinkedIn: ${linkedInComment()}`);
+        }
+
+    }());
 };
