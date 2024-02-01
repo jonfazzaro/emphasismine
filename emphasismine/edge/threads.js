613f1b4bc0054cfbb0580febef709118d66dda9d
Jon Fazzaro <jon@fazzaro.com>
2024-02-01 10:16:40 -0500

    try: threads token again

--- emphasismine/edge/threads.js
+++ emphasismine/edge/threads.js
@@ -3,11 +3,11 @@ const threads = require('@threadsjs/threads.js')
 module.exports = {
     post: async (params) => {
 
-        const client = new threads.Client();
-        await client.login(
-            process.env.threadsUsername,
-            process.env.threadsPassword
-        )
+        const client = new threads.Client({
+            token: process.env.threadsToken,
+            userAgent: process.env.threadsUserAgent,
+            appId: process.env.threadsAppId,
+        });
 
         await client.posts.create(process.env.threadsUserId, {contents: params.content})
             .catch(e => console.log(e))
