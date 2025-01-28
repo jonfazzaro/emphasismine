f0c986068b46f764c8eb8bae50c99c7c4e10c92d
Jon Fazzaro <jon@fazzaro.com>
2025-01-28 16:20:41 -0500

    fix: bad image URLs

--- emphasismine/edge/openGraph.js
+++ emphasismine/edge/openGraph.js
@@ -1,28 +1,42 @@
 const cheerio = require("cheerio");
 
-module.exports = { fetch: fetchMetadata };
+module.exports = {fetch: fetchMetadata};
 
 async function fetchMetadata(url) {
-  const response = await fetch(url)
-  const html = await response.text()
-  return parse(html, url)
+    const response = await fetch(url)
+    const html = await response.text()
+    return parse(html, url)
 }
 
-function parse(html, url) {
-  if (!html)
-    return {
-      url
-    };
+async function parse(html, url) {
+    if (!html)
+        return {
+            url
+        };
 
-  const $ = cheerio.load(html);
-  return {
-    url: og($, "url") || url,
-    title: og($, "title") || $("head title").text(),
-    description: og($, "description"),
-    image: og($, "image"),
-  };
+    const $ = cheerio.load(html);
+    return {
+        url: og($, "url") || url,
+        title: og($, "title") || $("head title").text(),
+        description: og($, "description"),
+        image: await ogImage($),
+    };
+}
+
+async function ogImage($) {
+    const url = og($, "image")
+    return await imageIsValid(url) ? url : null
+}
+
+async function imageIsValid(url) {
+    try {
+        await fetch(url)
+        return true
+    } catch (e) {
+        return false;
+    }
 }
 
 function og($, property) {
-  return $(`meta[property=og:${property}]`).attr("content");
+    return $(`meta[property=og:${property}]`).attr("content");
 }
